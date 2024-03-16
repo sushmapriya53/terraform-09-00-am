8f59e930cb31be81cc56f916be6fd4759f61b9ff
sushma <sushmapriyakothuru@gmail.com>
2024-03-16 16:36:35 +0530

    updates

--- day-5-import/main.tf
+++ day-5-import/main.tf
@@ -1,10 +1,8 @@
-resource "aws_instance" "dev" {
-    ami = "ami-02d7fd1c2af6eead0"
-    instance_type = "t3.micro"
-    tags = {
-      Name = "manual ec2"
-    }
-  
+provider "aws" {
+  region = "us-east-1" # Replace with your AWS region
 }
 
-  
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "sush.priya" # Replace with your bucket name
+  # Add other configuration parameters as needed
+}
