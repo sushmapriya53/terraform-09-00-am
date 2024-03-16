df894b459c72cd171a7af7ac1666f0b7c9c238b7
sushma <sushmapriyakothuru@gmail.com>
2024-03-16 11:25:59 +0530

    updates

---
+++ day-4-statefile-s3-dynamodb/main.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "dev2" {
+    bucket = "sush.priya"
+
+  
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+    name = "terraform-state-lock-dynamo"
+    hash_key = "LockID"
+    read_capacity = 20
+    write_capacity = 20
+
+    attribute {
+      name = "LockID"
+      type = "S"
+    }
+    }  
+
