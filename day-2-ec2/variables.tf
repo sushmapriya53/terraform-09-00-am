fac13fd5876f5de1c8a5b530985e02e0c9b57e19
sushma <sushmapriyakothuru@gmail.com>
2024-03-21 14:49:14 +0530

    updates

---
+++ day-2-ec2/variables.tf
@@ -0,0 +1,18 @@
+variable "ami_id" {
+    description = "passing ami value to main.tf"
+    type = string
+    default = "ami-02d7fd1c2af6eead0"
+
+  
+}
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+
+  
+}
+variable "key_name" {
+    type = string
+    default = "serverkey"
+  
+}
